03238a5693c7fd2e489b2c9fed5346393b8656e2
liujm <liujm@acrabsoft.com>
2023-10-08 17:49:25 +0800

    init project

---
+++ src/main/java/cn/ctrl_cv/laz_person/LazPersonApplication.java
@@ -0,0 +1,13 @@
+package cn.ctrl_cv.laz_person;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazPersonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LazPersonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/ctrl_cv/laz_person/LazPersonApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ctrl_cv.laz_person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazPersonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
